2c92d8d314be2591de5423979466d80f14598a8d
Matias Said <ma1000.ms@fpuna.edu.py>
2024-08-30 20:53:34 -0400

    cambie el saludo y saludo a Mica

--- main.c
+++ main.c
@@ -1,6 +1,7 @@
 #include <stdio.h>
 
 void main(void) {
-	printf("Hola, mundo!");
+	printf("Hola, FPUNA! :)");
+	printf("Mickaelaa la Vice y mizz!"); //No se como se escribre tu nombre jeje
 	return 0;
 }
