1180875165123cfb649f90f105b6d671d18e175e
Matias Said <ma1000.ms@fpuna.edu.py>
2024-08-30 21:09:41 -0400

    agregue opciones y un saludo

--- main.c
+++ main.c
@@ -1,7 +1,18 @@
 #include <stdio.h>
 
 void main(void) {
-	printf("Hola, FPUNA! :)");
-	printf("Mickaelaa la Vice y mizz!"); //No se como se escribre tu nombre jeje
-	return 0;
+    int opcion;
+
+    printf("Elige una opción (1 o 2): ");
+    scanf("%d", &opcion);
+
+    if(opcion == 1) {
+        printf("Hola, FPUNA uwu! :)\n");
+    } else if(opcion == 2) {
+        printf("Mickaela la Vice y mizzuna !\n"); // No se si escribi bien tu nombre
+    } else {
+        printf("Opción Incorrecta\n");
+    }
+
+    return;
 }
